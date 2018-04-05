d1670bcaf8fb9e8fef5adf0c99fffaeab7aad65e
MariaTikhonova <ritz.rkraft@gmail.com>
2018-04-05 21:31:28 +0300

    Added product views and devise users stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_sanitized_parameters, if: :devise_controller?
+
+
+protected 
+
+def configure_sanitized_parameters
+	devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+	  user_params.permit(:name, :email, :password, :password_confirmation, :image)
+	end
+
+	devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+	  user_params.permit(:name, :email, :password, :password_confirmation, :image)
+	end
+
+	devise_parameter_sanitizer.permit(:account_update) do |user_params|
+		user_params.permit(:name, :email, :password, :password_confirmation, :current_password, :image)
+	end
+end
+
 end
