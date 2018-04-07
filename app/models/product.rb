60f972934e832ce2786b986d8b50637e69595d61
MariaTikhonova <ritz.rkraft@gmail.com>
2018-04-07 14:45:00 +0300

    Cart relations, items and some tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,22 @@
 class Product < ApplicationRecord
 	validates :title, :description, :image_url, presence: true
-	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates :title, uniqueness: true
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates :image_url, allow_blank: true, format: {
 		with: %r{\.(gif|jpg|png)\Z}i,
 	    message: 'Must be a gif, jpg or png image!' }
+
+	has_many :line_items
+
+	before_destroy :ensure_not_referenced_by_any_line_item 
+
+	private
+
+	def ensure_not_referenced_by_any_line_item
+	unless line_items.empty?
+		errors.add(:base, 'Line Items Present')
+		throw :abort
+	 end
+    end
+
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -37,12 +37,18 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
     assert_redirected_to product_url(@product)
   end
+  
+  test "can't delete product in the cart" do
+     assert_difference('Product.count', 0) do
+      delete_product_url(products(:two))
+     end
+    assert_redirected_to products_url
+   end
 
   test "should destroy product" do
     assert_difference('Product.count', -1) do
       delete product_url(@product)
     end
-
-    assert_redirected_to products_url
+     assert_redirected_to products_url
   end
 end
