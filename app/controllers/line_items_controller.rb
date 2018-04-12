81e55484d584282f42d7cf8feddf48e0bb749f97
MariaTikhonova <ritz.rkraft@gmail.com>
2018-04-12 22:16:54 +0300

    added quantity of items to cart, 2 migrations

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,7 +28,7 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
      product = Product.find(params[:product_id])
-     @line_item = @cart.line_items.build(product: product)
+     @line_item = @cart.add_product(product: product)
 
     respond_to do |format|
       if @line_item.save
@@ -47,7 +47,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.update(line_item_params)
         format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @line_item }
+        format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :edit }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
