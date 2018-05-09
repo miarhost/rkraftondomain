9c596f3a965c47ae5bca288696914520ceeea6b1
MariaTikhonova <ritz.rkraft@gmail.com>
2018-05-09 22:22:32 +0300

    Inquiry Mailer

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -2,7 +2,7 @@ class InquiryMailer < ApplicationMailer
 	default from: "rghostme@gmail.com"
 
 	def inquire(email_address)
-		mail to:(email_address, subject: "Your inquiry is sent")
+		mail(to: email_address, subject: "Your inquiry is sent")
 	end
 
 end
