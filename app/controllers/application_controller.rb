a0c876058efe53307dcb7a438d4c0c57d28c127b
MariaTikhonova <ritz.rkraft@gmail.com>
2018-05-14 22:49:00 +0300

    i18n fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,9 @@ def set_i18n_locale_from_params
 	if params[:locale]
 		if I18n.available_locales.map(&:to_s).include?(params[:locale])
 			I18n.locale = params[:locale]
+		else
+		 flash[:notice] = "#{params[:locale]} translation not available"
+		 logger.error flash.now[:notice]
 		end
 	 end
 end
