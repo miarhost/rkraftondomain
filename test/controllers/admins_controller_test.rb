0bc5dba6b6f45ec3d4aeccec7537c4273baf131f
MariaTikhonova <ritz.rkraft@gmail.com>
2018-05-17 19:24:07 +0300

    i18n & admin controller test

--- test/controllers/admins_controller_test.rb
+++ test/controllers/admins_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class AdminsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+setup do
+line_item = line_items(:one)	
+end
+
+test "should get index" do
+	get admins_url
+	assert_responce :success
+end
+
+test "should get orders number" do
+  assert_equal('LineItems.count') do
+  end
+end
+
 end
