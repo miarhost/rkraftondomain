492e09ad1dd87729c6ecd34f2a100d00d3dc06b2
MariaTikhonova <ritz.rkraft@gmail.com>
2018-05-18 18:03:56 +0300

    Griddler Cloudmailin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -108,7 +108,7 @@ config.email_class = Griddler::Email
 config.processor_method = :process
 config.reply_delimiter = '--REPLY ABOVE THIS LINE--'
 config.email_service = :cloudmailin
-config.email = {
+      to: {
   host: "imap.gmail.com",
   port: 993,
   domain: ENV["GMAIL_DOMAIN"],
