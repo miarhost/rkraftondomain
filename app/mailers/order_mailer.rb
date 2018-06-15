54e8670729e7ce115dfe514bb73a47ffa6a2ab34
MariaTikhonova <ritz.rkraft@gmail.com>
2018-06-15 18:23:49 +0300

    I18n translations order mailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -6,7 +6,7 @@ default from: "rghostme@gmail.com"
   
   def received(order)
      @order = order
-     mail to: order.email, subject: 'Your order is received and at work'
+     mail to: order.email, subject: t('.subject')
   end
 
   def shipped(order)
